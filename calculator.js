ef4bda04a509e695423511a7c68be968367bb792
Adib Faqih <foezst@gmail.com>
2020-04-02 13:30:21 +0700

    created Calculetor class

--- calculator.js
+++ calculator.js
@@ -2,21 +2,41 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (number) {
+    this.result = number;
   }
-  add () {
+  add (num) {
+    if (num)
+    this.result += num;
+    return this;
   }
-  subtract () {
+  subtract (num) {
+    this.result -= num;
+    return this;
   }
-  multiply () {
+  multiply (num) {
+    this.result *= num;
+    return this;
   }
-  divide () {
+  divide (num) {
+    this.result /= num;
+    return this;
   }
   square () {
+    this.result *= this.result;
+    return this;
   }
   squareRoot () {
+    this.result = Math.sqrt(this.result);
+    return this;
   }
 }
+
+//example
+const calc = new Calculator(9);
+console.log(calc.result);
+calc.squareRoot().add(calc.result).divide(2).multiply(8).add(3).square().subtract(7);
+console.log(calc.result);
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
