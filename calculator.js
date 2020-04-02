20a2cf5aea25793210bd981bdb3632286d1c28d8
chabode <rizqa.aulia@gmail.com>
2020-04-02 15:10:06 +0700

    calculator done

--- calculator.js
+++ calculator.js
@@ -2,19 +2,45 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (number1) {
+    this.number1 = number1
+    // this.number2 = number2
+    // this.addNum = this.add(num)
   }
-  add () {
+  add (num) {
+    console.log(`Angka ${this.number1} ditambah ${num} = ${this.number1 + num}`)
+    this.number1 += num
+    return this
   }
-  subtract () {
+  subtract (num) {
+    console.log(`Angka ${this.number1} dikurang ${num} = ${this.number1 - num}`)
+    this.number1 -= num
+    return this
   }
-  multiply () {
+  multiply (num) {
+    console.log(`Angka ${this.number1} dikali ${num} = ${this.number1 * num}`)
+    this.number1 *= num
+    return this
   }
-  divide () {
+  divide (num) {
+    console.log(`Angka ${this.number1} dibagi ${num} = ${this.number1 / num}`)
+    this.number1 /= num
+    return this
   }
-  square () {
+  square (num) {
+    console.log(`Angka ${this.number1} dikuadrat ${num} = ${Math.pow(this.number1, num)}`)
+    this.number1 = Math.pow(this.number1, num)
+    return this
   }
   squareRoot () {
+    console.log(`Akar dari Angka ${this.number1} = ${Math.sqrt(this.number1)}`)
+    this.number1 = Math.sqrt(this.number1)
+    return this
+  }
+  circleArea(){
+    console.log(`Luas Lingkaran dengan radius ${this.number1} = ${Math.PI*Math.pow(this.number1,2)}`)
+    this.number1 = Math.PI*Math.pow(this.number1,2)
+    return this
   }
 }
 
