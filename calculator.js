45a4484fa29abbe81cbc2bafcc7099aaa476c4ab
fadhlanfm <fafadhlanriz@gmail.com>
2020-04-02 17:42:07 +0700

    Release 0: OK

--- calculator.js
+++ calculator.js
@@ -2,21 +2,60 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (num = 0) {
+    this.num = num
   }
-  add () {
+
+  add (num) {
+    let result = this.num + num
+    console.log(`${this.num} + ${num} = ${result}`)
+    this.num = result
+    return this
   }
-  subtract () {
+
+  subtract (num) {
+    let result = this.num - num
+    console.log(`${this.num} - ${num} = ${result}`)
+    this.num = result
+    return this
   }
-  multiply () {
+
+  multiply (num) {
+    let result = this.num * num
+    console.log(`${this.num} * ${num} = ${result}`)
+    this.num = result
+    return this
   }
-  divide () {
+
+  divide (num) {
+    let result = this.num / num
+    console.log(`${this.num} / ${num} = ${result}`)
+    this.num = result
+    return this
   }
-  square () {
+
+  square (num) {
+    let result = this.num ** num
+    console.log(`${this.num} to the power of ${num} = ${result}`)
+    this.num = result
+    return this
   }
+
   squareRoot () {
+    let result = Math.sqrt(this.num)
+    console.log(`Squared root of ${this.num} = ${result}`)
+    this.num = result
+    return this
   }
+
+  pi(){
+    return Math.PI
+  }
+
 }
+
+let operation = new Calculator()
+operation.add(4).divide(2).multiply(8).squareRoot().square(3).add(operation.pi())
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
