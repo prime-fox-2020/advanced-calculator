2262c4da475b0d570ce4791d373445283df0d093
rinotoharto <rinotoharto@gmail.com>
2020-04-02 13:15:44 +0700

    calculator done

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 'use strict'
 
+/* 
+Method dalam Calculator :
+- .add() << Method untuk melakukan proses penjumlahan dari nilai awal anda
+- .subsctract() << Method untuk melakukan proses pengurangan dari nilai awal anda
+- .multiply() << Method untuk melakukan proses perkalian dari nilai awal anda
+- .divide() << Method untuk melakukan proses pembagian dari nilai awal anda
+- .square() << Method untuk melakukan proses pangkat dari nilai awal anda
+- .squareRoot() << Method untuk mencari akar pangkat dari nilai awal sesuai dengan input anda
+- .pi() << Method untuk proses perkalian nilai awal dengan pi
+- .circle() << Method untuk menghitung luas lingkaran dengan nilai input awal sebagai diameter
+*/
+
 const Calculator = require("./calculator.js")
 
-// execute function on calculator.js in here
+let a = new Calculator() /* <<<< Input Nilai awal anda */
+
+
+
+
+
+
