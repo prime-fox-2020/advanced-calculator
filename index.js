3f80ffef6b5bad9d7967c3ed9e4de8c7c61faff1
Rozaan3309 <Rozaan3309@users.noreply.github.com>
2020-04-02 16:09:50 +0700

    advanced calculator done

--- index.js
+++ index.js
@@ -3,3 +3,8 @@
 const Calculator = require("./calculator.js")
 
 // execute function on calculator.js in here
+let instance = new Calculator
+
+instance.add(2).squareRoot(2).circleArea()
+
+console.log(instance)
