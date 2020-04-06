869edf9cd99445a12004ad6c56b2f4bb5d969d74
permaficus <abukhalif2019@gmail.com>
2020-04-06 20:21:38 +0700

    Semi Advanced Calculator Done !

--- calculator.js
+++ calculator.js
@@ -2,19 +2,41 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (sum = 0) {
+    this.sum = sum;
   }
-  add () {
+  add = ( value = 0 ) => {
+    this.sum += value;
+    return this;
   }
-  subtract () {
+  subtract = ( value = 1 ) => {
+    this.sum -= value;
+    return this;
   }
-  multiply () {
+  multiply = ( value = 1 ) => {
+    this.sum *= value;
+    return this;
   }
-  divide () {
+  divide = ( value = 1 ) => {
+    this.sum /= value;
+    return this;
   }
-  square () {
+  square = ( value = 2 ) => {
+    this.sum = Math.pow(this.sum, value);
+    return this;
   }
-  squareRoot () {
+  squareRoot = () => {
+      this.sum = Math.sqrt(this.sum);
+      return this;
+  }
+
+  radius = value => {
+    this.sum = Math.PI * Math.pow( value, 2 )
+    return this;
+  }
+
+  total = () => {
+    return `Total : ${this.sum}`
   }
 }
 
--- index.js
+++ index.js
@@ -3,3 +3,8 @@
 const Calculator = require("./calculator.js")
 
 // execute function on calculator.js in here
+let calc = new Calculator();
+calc.add(5).multiply(2).subtract(2);
+console.log(calc.total())
+calc.radius(9);
+console.log(calc.total())
